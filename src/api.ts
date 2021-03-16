b8be41f051221e2e0c9f256c59e8dd685cfb5253
Greene <t0937383@gmail.com>
2021-03-16 11:05:24 +0800

    sort out

--- src/api.ts
+++ src/api.ts
@@ -1,1 +1,42 @@
+<<<<<<< HEAD
 const url = "http://script.google.com/[....PUT YOUR SCRIPT URL HERE....]/exec";
+=======
+import axios from "axios";
+
+const baseRequest = axios.create({
+  baseURL: "https://script.google.com/macros/s/",
+});
+
+enum Sheet {
+  test = "AKfycbzFWdgPeex3jjuP3oQU7_6aZz_G6iEalRW9y6EuJoJgaoNq__W0jYRI",
+}
+
+enum Method {
+  Get = "get",
+  Post = "post",
+}
+
+type createApiArg = {
+  sheetID: Sheet;
+  method: Method;
+  payload?: {};
+};
+
+const createAPI = ({
+  sheetID,
+  method,
+  payload = undefined,
+}: createApiArg) => async () => {
+  switch (method) {
+    case Method.Get:
+      return await baseRequest[method](`${sheetID}/exec`);
+    case Method.Post:
+      return await baseRequest[method](`${sheetID}/exec`, payload);
+  }
+};
+
+export const getSheet = createAPI({
+  sheetID: Sheet.test,
+  method: Method.Get,
+});
+>>>>>>> dev
--- src/bot.ts
+++ src/bot.ts
@@ -4,6 +4,10 @@ import AOS from "./setting/AOS";
 import WH40K from "./setting/WH40K";
 import INF from "./setting/INF";
 import { PollOpts, PollOptsString, Game, Rule } from "./enum";
+<<<<<<< HEAD
+=======
+import { getSheet } from "./api";
+>>>>>>> dev
 
 const url =
   "https://www.warhammer-community.com/2021/01/24/sunday-preview-angels-of-darkness/";
@@ -29,17 +33,24 @@ export const awakeTheodore = (key: string) => {
   });
   // crawler(url);
 
+<<<<<<< HEAD
   client.on("message", (msg) => {
     if (!msg.guild || !msg.member) return; // 不是公會、不是會員就不反應
     if (msg.member?.user.bot) return; // 是 Bot 就不反應
+=======
+  client.on("message", async (msg) => {
+    if (!msg.guild || !msg.member) return; // 不是公會、不是會員就不反應
+    if (msg.author.bot) return; // 是 Bot 就不反應
+>>>>>>> dev
     if (isTextCh(msg.channel) === false) return; // 不是文字訊息就不反應
     if (msg.content.substring(0, prefix.length) !== prefix) return; // 沒有前綴詞就不反應
 
-    const memberID = msg.member?.id;
+    const authorID = msg.author.id;
     const chID = msg.channel.id;
     const [firstCMD, ...otherCmd] = msg.content
       .substring(prefix.length)
       .split(" ");
+<<<<<<< HEAD
 
     if (firstCMD === Game.aos || firstCMD === Game.AOS) {
       const ruleType = otherCmd.join("");
@@ -72,11 +83,28 @@ export const awakeTheodore = (key: string) => {
           break;
         case Rule.all:
           content = wh40k.getAllRules();
+=======
+
+    if (firstCMD === Game.aos || firstCMD === Game.AOS) {
+      const ruleType = otherCmd.join("");
+      const aos = new AOS();
+      let content = {};
+      switch (ruleType) {
+        case Rule.faction:
+          content = aos.getFactionRules();
+          break;
+        case Rule.global:
+          content = aos.getGlobalRules();
+          break;
+        case Rule.all:
+          content = aos.getAllRules();
+>>>>>>> dev
       }
       msg.channel.send(content);
       return;
     }
 
+<<<<<<< HEAD
     if (firstCMD === Game.inf || firstCMD === Game.INF) {
       const ruleType = otherCmd.join("");
       const inf = new INF();
@@ -84,11 +112,31 @@ export const awakeTheodore = (key: string) => {
       switch (ruleType) {
         case Rule.all:
           content = inf.getRules();
+=======
+    if (firstCMD === Game.wh40k || firstCMD === Game.WH40K) {
+      const ruleType = otherCmd.join("");
+      const wh40k = new WH40K();
+      let content = {};
+      switch (ruleType) {
+        case Rule.faction:
+          content = wh40k.getFactionRules();
+          break;
+        case Rule.global:
+          content = wh40k.getGlobalRules();
+          break;
+        case Rule.all:
+          content = wh40k.getAllRules();
+          break;
+        case Rule.stratagem:
+          content = wh40k.getCoreStratagem();
+          break;
+>>>>>>> dev
       }
       msg.channel.send(content);
       return;
     }
 
+<<<<<<< HEAD
     if (firstCMD === "指令") {
       msg.channel.send({
         embed: {
@@ -126,6 +174,57 @@ export const awakeTheodore = (key: string) => {
 
       if (options.length < 2 || options.length > 26) return;
 
+=======
+    if (firstCMD === Game.inf || firstCMD === Game.INF) {
+      const ruleType = otherCmd.join("");
+      const inf = new INF();
+      let content = {};
+      switch (ruleType) {
+        case Rule.all:
+          content = inf.getRules();
+      }
+      msg.channel.send(content);
+      return;
+    }
+
+    if (firstCMD === "指令") {
+      msg.channel.send({
+        embed: {
+          title: "指令集",
+          fields: [
+            {
+              name: "遊戲規則",
+              value: `
+              指令:[遊戲名稱] [規則/陣營規則/通用規則]
+              範例:"AOS 陣營規則"
+              `,
+            },
+            {
+              name: "發起投票",
+              value: `
+              指令:投票 [選項A,選項B,...目前最多支援26個選項]
+              `,
+            },
+          ],
+        },
+      });
+      return;
+    }
+
+    if (firstCMD === "投票" && otherCmd) {
+      const rawString = otherCmd.join(" ");
+
+      if (rawString[0] !== "[") return;
+      if (rawString[rawString.length - 1] !== "]") return;
+
+      const preFormat = rawString.slice(1, rawString.length - 1).split(",");
+      const options = Array.from(new Set(preFormat)).filter(
+        (opt) => opt !== "" && opt !== ","
+      );
+
+      if (options.length < 2 || options.length > 26) return;
+
+>>>>>>> dev
       const getUppercase = (idx: number) =>
         String.fromCharCode(65 + idx) as PollOptsString;
 
@@ -147,18 +246,32 @@ export const awakeTheodore = (key: string) => {
       return;
     }
 
+<<<<<<< HEAD
     if (firstCMD === "新品預購" && otherCmd) {
       if (memberID !== Greene) {
+=======
+    if (firstCMD === "新品預購") {
+      if (authorID !== Greene) {
+>>>>>>> dev
         msg.reply("只有 Greene 才能發佈新品預購喔");
         return;
       }
 
+<<<<<<< HEAD
       const rawString = otherCmd.join("");
 
       if (rawString[0] !== "[") return;
       if (rawString[rawString.length - 1] !== "]") return;
 
       const preFormat = rawString.slice(1, rawString.length - 1).split(",");
+=======
+      const rawString = otherCmd.join(" ");
+
+      if (rawString.startsWith("[") === false) return;
+      if (rawString.endsWith("]") === false) return;
+
+      const preFormat = rawString.slice(1, -1).split(",");
+>>>>>>> dev
       const options = Array.from(new Set(preFormat)).filter(
         (opt) => opt !== "" && opt !== ","
       );
@@ -173,7 +286,25 @@ export const awakeTheodore = (key: string) => {
       });
 
       const embed = new Discord.MessageEmbed();
+<<<<<<< HEAD
       embed.setTitle("來啦！是新品預購！").addField("選項", content);
+=======
+      embed
+        .setTitle("來啦！新品預購！")
+        .setDescription(
+          `
+          【注意】
+          這是為了讓新品能夠準時在發售日送到指揮官手上的超前部署，我們沒辦法在這個時間點公布商品的價格。所以很介意價格的指揮官，建議等到本週末 GW 在官網公布後再與小丘訂購，可是這樣就會沒辦法在第一時間獲得商品。還請各位指揮官慎重考慮。
+
+          ＊發售時間為下週六＊
+          ＊官網限定與 FW 仍沒辦法享有折扣＊
+        `
+        )
+        .setColor("#0099ff")
+        .addField("連結", "https://discord.js.org/")
+        .addField("選項", content)
+        .setFooter(`###今天 ${new Date().getDate()} 號，下午兩點半結單###`);
+>>>>>>> dev
 
       msg.channel
         .send(embed)
@@ -185,16 +316,43 @@ export const awakeTheodore = (key: string) => {
         })
         .catch(console.error);
       return;
+<<<<<<< HEAD
     }
 
     if (firstCMD === "還不快歡呼") {
       msg.channel.send("Yeeeeeeah!");
     }
 
+=======
+    }
+
+    if (firstCMD === "還不快歡呼") {
+      msg.channel.send("Yeeeeeeah!");
+      return;
+    }
+
+>>>>>>> dev
     if (firstCMD.includes("我")) {
       msg.channel.send(`我不會「${firstCMD.replace("我", "你")}」`);
       return;
     }
+<<<<<<< HEAD
+=======
+
+    if (firstCMD == "GASAPI測試") {
+      const startTime = new Date().getTime();
+      msg.reply(`測試開始`);
+      await getSheet()
+        .then((res) =>
+          msg.reply(`測試成功，回傳結果: ${JSON.stringify(res.data)}`)
+        )
+        .catch((error) => msg.reply(`測試失敗，錯誤訊息: ${error}`));
+      const endTime = new Date().getTime();
+      msg.reply(`測試結束，間隔${endTime - startTime}毫秒`);
+      return;
+    }
+
+>>>>>>> dev
     msg.channel.send(`我不會「${firstCMD}」`);
 
     // switch (firstCMD) {
